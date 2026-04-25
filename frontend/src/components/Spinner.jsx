43d99dbc89f2254f93d65ee09dbbe21e71b8fd91
yash <yashg5304@gmail.com>
2026-04-25 20:48:45 +0530

    loading height fixed

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Spinner() {
     return (
 
-        <div className="flex justify-center items-center h-40">
+        <div className="flex justify-center items-center h-screen">
             <div className="w-10 h-10 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
         </div>
 
